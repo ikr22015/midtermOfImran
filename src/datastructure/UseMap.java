c71583b45b725eb77f3cd874b4c88b19f35a0729
Imran Kabir <ikr22015@gmail.com>
2017-12-19 19:52:30 -0500

    Map and Queue

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,13 @@
 package datastructure;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.security.Key;
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -11,6 +19,41 @@ public class UseMap {
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
 
-	}
+		// Create map to store
+		Map<String, List<String>> map = new HashMap<String, List<String>>();
 
+		// create list one and store values
+		List<String> valuSetOne = new ArrayList<String>();
+		valuSetOne.add("Apple");
+		valuSetOne.add("Banana");
+
+		// Create list two and store values
+		List<String> valuSetTwo = new ArrayList<String>();
+		valuSetTwo.add("Orange");
+		valuSetTwo.add("Watermelon");
+
+		// Create list three and store values
+		List<String> valuSetThree = new ArrayList<String>();
+		valuSetThree.add("Mango");
+		valuSetThree.add("Pineapple");
+
+		// Put values into the map
+		map.put("A", valuSetOne);
+		map.put("B", valuSetTwo);
+		map.put("C", valuSetThree);
+
+		//Retrieve values with for each loop
+		for(Map.Entry<String,List<String>> entry: map.entrySet()){
+			String key = entry.getKey();
+			List<String> values = entry.getValue();
+			System.out.println(key+" " + values);
+		}
+
+		Iterator itr = map.entrySet().iterator();
+		//Retrieve values with while loop
+		while (itr.hasNext()){
+			Map.Entry elements = (Map.Entry)itr.next();
+			System.out.println(elements.getKey()+" "+elements.getValue());
+		}
+	}
 }
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -9,6 +12,36 @@ public class UseQueue {
 		 * 
 		 */
 
-	}
+		//Demonstrate Queue
+		PriorityQueue<String> queue = new PriorityQueue<>();
+		//Add elements
+		queue.add("Aam");
+		queue.add("Jaam");
+		queue.add("Katal");
+		queue.add("Lichu");
+		queue.add("Peyara");
 
+		//Retrieving all elements usung while loop
+		Iterator itr = queue.iterator();
+		while (itr.hasNext()){
+			System.out.println(itr.next());
+		}
+
+		//Peek head queue element
+		String headQueue = queue.peek();
+		System.out.println("Head of the queue is: "+headQueue);
+
+		//Remove queue by an object
+		queue.remove("Peyara");
+		//Remove head of queue
+		queue.remove();
+		//Poll head of queue
+		queue.poll();
+		System.out.println("After removing three elements:");
+
+		//Retrieve all elements using for each loop
+		for (String it: queue){
+			System.out.println(it);
+		}
+	}
 }
