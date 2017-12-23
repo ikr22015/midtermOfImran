fa6b2a69190d210db556cc66f8be9d7aa3423e4d
Imran Kabir <ikr22015@gmail.com>
2017-12-22 23:12:13 -0500

    Edited few things.

--- src/myDesign/PensionCalculation.java
+++ src/myDesign/PensionCalculation.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class PensionCalculation {
-    public static int calculatePension(int salary, String dateOfJoining){
-        LocalDate start = LocalDate.of(1980,9,7);
+    public static int calculatePension(int salary){
+        LocalDate start = LocalDate.of(2017,9,7);
         //LocalDate end = LocalDate.of(2014, 2, 28); // use for age-calculation: LocalDate.now()
         LocalDate today = LocalDate.now();
         long jobAge = ChronoUnit.YEARS.between(start, today);
